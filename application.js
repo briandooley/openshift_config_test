d0c2884b611d486a8ce39fde9d6d22e8e3b22b0e
Brian Dooley <brian.dooley@feedhenry.com>
2018-10-11 15:35:43 +0100

    more adjustments

--- application.js
+++ application.js
@@ -5,3 +5,8 @@ var port = process.env.FH_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8002;
 var host = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 console.log(new Date() + " Environment variable is " + process.env.TEST_MESSAGE);
+
+app.listen(port, host, function() {
+  console.log("App started at: " + new Date() + " on port: " + port);
+});
+
